40b32e596e8218e2caf6f91f53597cd4e1abb20d
Oroghene Emudainohwo <73497649+oroghene@users.noreply.github.com>
2026-06-14 21:13:06 -0700

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        # backtracking is the only way for normal person to solve this
+        # 2 choices in backtracking, choose this and continue to next
+        # or skip this and continue to next (choose from unchosen numbers)
+        # backtracking builds permutations by choosing numbers one by one 
+        # and exploring all possible orders
+        # at every step
+        # good and only base case
+        # at a full permutation, (check length), add to result
+        # first choice
+        # pick this number that has not been used yet
+        # add it to the current permutation
+        # recursively continue building
+        # second choice
+        # backtrack (remove this number) and pick next number
+
+        res: Set[Tuple[int]] = set()
+        n: int = len(nums)
+        picked: List[bool] = [False] * n
+        def backtrack(perm: List[int], picked: List[bool]) -> None:
+            # good and only base case
+            if len(perm) == len(nums):
+                res.add(tuple(perm[::]))
+                return
+            for i in range(len(nums)):
+                # first choice
+                if not picked[i]:
+                    perm.append(nums[i])
+                    picked[i] = True
+                    # second choice
+                    backtrack(perm, picked)
+                    perm.pop()
+                    picked[i] = False
+
+        backtrack([], picked)
+        return list(res)
