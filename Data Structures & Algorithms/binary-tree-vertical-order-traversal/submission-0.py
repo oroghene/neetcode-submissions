f625f4e2c1e5eb2e6176ac92da06fb8c854c8342
Oroghene Emudainohwo <73497649+oroghene@users.noreply.github.com>
2026-06-19 14:01:55 -0700

    Add: binary-tree-vertical-order-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-vertical-order-traversal/submission-0.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        cols = defaultdict(list)
+        q = deque([(root, 0)])
+        while q:
+            node, col = q.popleft()
+            if node:
+                cols[col].append(node.val)
+                q.append((node.left, col - 1))
+                q.append((node.right, col + 1))
+        return [cols[x] for x in sorted(cols)]
